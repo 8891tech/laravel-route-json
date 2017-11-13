915dc4850d5f1f6c441c26a7a879745b242465b3
黎明 <treerootboy@gmail.com>
2017-11-13 13:27:54 +0800

    Fix Some Bug

--- src/Console/RouteJsonCommand.php
+++ src/Console/RouteJsonCommand.php
@@ -3,7 +3,7 @@ namespace T8891\RouteJson\Console;
 
 use Illuminate\Console\Command;
 
-class RouteListCommand extends Command
+class RouteJsonCommand extends Command
 {
     protected $name = 'route:json';
 
@@ -17,12 +17,13 @@ class RouteListCommand extends Command
     {
         $output_path = $this->option('output') ?: base_path().'/public';
         $routeCollection = $this->laravel ? $this->laravel->getRoutes() : $this->app->getRoutes();
+
         $routeJson = array();
         foreach($routeCollection as $route) {
             $routeJson[] = array(
-                'path' => $route['path']
+                'path' => $route['uri']
             );
         }
-        file_put_contents($output_path, json_encode($routeJson));
+        file_put_contents($output_path.'/route.json', json_encode($routeJson, JSON_UNESCAPED_SLASHES));
     }
 }
--- src/RouteJsonServiceProvider.php
+++ src/RouteJsonServiceProvider.php
@@ -5,10 +5,14 @@ use Illuminate\Support\ServiceProvider;
 
 class RouteJsonServiceProvider extends ServiceProvider
 {
+    protected $defer = true;
+
     public function register()
     {
         $this->app->singleton('command.route.json', function ($app) {
             return new Console\RouteJsonCommand();
         });
+
+        $this->commands(array('command.route.json'));
     }
 }
