3cd8f0069cee6186bd83cc85cf2f12e10adb78d6
黎明 <treerootboy@gmail.com>
2017-11-13 14:58:07 +0800

    compatible Laravel and Lumen

--- src/Console/RouteJsonCommand.php
+++ src/Console/RouteJsonCommand.php
@@ -2,26 +2,30 @@
 namespace T8891\RouteJson\Console;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
 
 class RouteJsonCommand extends Command
 {
     protected $name = 'route:json';
 
-    protected $signature = 'route:json
-                               {--o|output= : Specify the json file save path}
-                           ';
-
     protected $description = 'Export all registered routes to a json file';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->addOption('output', 'o', InputOption::VALUE_OPTIONAL, 'Specify the json file save path');
+    }
 
     public function fire()
     {
         $output_path = $this->option('output') ?: base_path().'/public';
-        $routeCollection = $this->laravel ? $this->laravel->getRoutes() : $this->app->getRoutes();
+
+        $routes = class_exists('\Route') ? \Route::getRoutes() : $this->laravel->getRoutes();
 
         $routeJson = array();
-        foreach($routeCollection as $route) {
+        foreach($routes as $route) {
             $routeJson[] = array(
-                'path' => $route['uri']
+                'path' => class_exists('\Route') ? $route->uri() : $route['uri']
             );
         }
         file_put_contents($output_path.'/route.json', json_encode($routeJson, JSON_UNESCAPED_SLASHES));
