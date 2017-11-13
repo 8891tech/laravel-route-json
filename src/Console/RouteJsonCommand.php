40236e530ddc324f80645bbd9ba48b7fece2ddcb
黎明 <treerootboy@gmail.com>
2017-11-13 17:24:14 +0800

    Add extra route config except registered route

--- src/Console/RouteJsonCommand.php
+++ src/Console/RouteJsonCommand.php
@@ -14,20 +14,40 @@ class RouteJsonCommand extends Command
     {
         parent::__construct();
         $this->addOption('output', 'o', InputOption::VALUE_OPTIONAL, 'Specify the json file save path');
+
     }
 
     public function fire()
     {
         $output_path = $this->option('output') ?: base_path().'/public';
-
-        $routes = class_exists('\Route') ? \Route::getRoutes() : $this->laravel->getRoutes();
+        $routes = $this->isLumen() ? $this->app->getRoutes() : \Route::getRoutes();
 
         $routeJson = array();
         foreach($routes as $route) {
             $routeJson[] = array(
-                'path' => class_exists('\Route') ? $route->uri() : $route['uri']
+                'path' => method_exists($route, 'uri') ? $route->uri() : $route['uri']
             );
         }
+
+        $extra = $this->app['config']->get('routejson.extra');
+        if (is_array($extra) && count($extra)) {
+            foreach($extra as $route) {
+                $routeJson[] = array('path' => $route);
+            }
+        }
+
         file_put_contents($output_path.'/route.json', json_encode($routeJson, JSON_UNESCAPED_SLASHES));
+    }
+
+    protected function isLumen()
+    {
+        if (!property_exists($this, 'app')) {
+            $this->app = $this->laravel;
+        }
+        $versionMethod = 'version';
+        if (!method_exists($this->app, $versionMethod)) {
+            $versionMethod = 'getVersion';
+        }
+        return str_contains($this->app->{$versionMethod}(), 'Lumen');
     }
 }
--- src/RouteJsonServiceProvider.php
+++ src/RouteJsonServiceProvider.php
@@ -5,14 +5,26 @@ use Illuminate\Support\ServiceProvider;
 
 class RouteJsonServiceProvider extends ServiceProvider
 {
-    protected $defer = true;
+
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/routejson.php';
+        $this->publishes(array($configPath => $this->configPath()));
+    }
 
     public function register()
     {
+        $configPath = __DIR__ . '/../config/routejson.php';
+        $this->mergeConfigFrom($configPath, 'routejson');
         $this->app->singleton('command.route.json', function ($app) {
             return new Console\RouteJsonCommand();
         });
 
         $this->commands(array('command.route.json'));
+    }
+
+    public function configPath()
+    {
+        return base_path().'/config/routejson.php';
     }
 }
